16a8582286323db455c0e2beab4e1c06735e09a4
Justin <124533023+Stauber-Justin@users.noreply.github.com>
2025-06-16 10:25:43 +0200

    Add serialization using directive

--- WDBXEditor/Storage/Database.cs
+++ WDBXEditor/Storage/Database.cs
@@ -12,6 +12,7 @@ using System.Diagnostics;
 using System.Windows.Forms;
 using System.Text;
 using System.Web.Script.Serialization;
+using System.Web.Script.Serialization;
 
 namespace WDBXEditor.Storage
 {
